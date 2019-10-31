0e27a81c30a08bd9d89cc32bfa2fcbb985cf3938
nelsonmac <nelsonvargheseitp@gmail.com>
2019-10-31 22:07:14 +0100

    config modification

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + 'dist/grocery-angular'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist/grocery-angular', 'index.html'))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist/grocery-angular', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
