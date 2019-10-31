771c7bf568a9ab185a39c26ffe889a4c2b70b928
nelsonmac <nelsonvargheseitp@gmail.com>
2019-10-31 21:00:00 +0100

    server.js update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{grocery}}'));
+app.use(express.static('./dist/grocery-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{grocery}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/grocery-angular/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
